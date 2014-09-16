59c876bb055dad30167924f98155b957217a3db3
Nagaraj <nagarajr@thoughtworks.com>
2014-09-16 22:27:22 +0530

    Initial commit with Basic logging - Nagaraj

---
+++ HtmlBuildOutput/BuildLog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HtmlBuildOutput
+{
+	public class BuildLog
+	{
+		public BuildLog()
+		{
+			ProjectStartEvents = new List<ProjectStartLog>();
+			ProjectFinishEvents = new List<ProjectFinishLog>();
+			BuildErrors = new List<BuildError>();
+		}
+		public BuildStartLog BuildStartTime { get; set; }
+		public BuildFinishLog BuildEndTime { get; set; }
+
+		public List<ProjectStartLog> ProjectStartEvents { get; set; }
+		public List<ProjectFinishLog> ProjectFinishEvents { get; set; }
+
+		public List<BuildError> BuildErrors { get; set; } 
+	}
+
+	public class BuildError
+	{
+		public DateTime TimeStamp { get; set; }
+
+		public string ErrorCode { get; set; }
+
+		public string FileName { get; set; }
+
+		public int ColumnNumber { get; set; }
+
+		public int LineNumber { get; set; }
+
+		public string ProjectFile { get; set; }
+
+		public string Message { get; set; }
+
+		public string SubCategory { get; set; }
+	}
+
+	public class ProjectStartLog : BuildStartLog
+	{
+		public string ProjectFile { get; set; }
+	}
+
+
+	public class ProjectFinishLog : BuildFinishLog
+	{
+		public string ProjectFile { get; set; }
+	}
+
+	public class BuildFinishLog
+	{
+		public DateTime FinishTime { get; set; }
+
+		public bool IsBuildSucceeded { get; set; }
+
+		public string Message { get; set; }
+	}
+
+	public class BuildStartLog
+	{
+		public DateTime StartTime { get; set; }
+
+		public string Message { get; set; }
+	}
+}
---
+++ HtmlBuildOutput/LogGenerator.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace HtmlBuildOutput
+{
+    public class LogGenerator : Logger
+    {
+	    private static BuildLog buildLog;
+
+	    public LogGenerator()
+	    {
+		    buildLog = new BuildLog();
+	    }
+
+	    public override void Initialize(IEventSource eventSource)
+	    {
+		    eventSource.BuildStarted += OnBuildStarted; 
+			eventSource.BuildFinished += OnBuildFinished;
+			eventSource.ProjectStarted += OnProjectStarted;
+			eventSource.ProjectFinished += OnProjectFinished;
+			eventSource.ErrorRaised += OnErrorRaised;
+	    }
+
+	    private void OnErrorRaised(object sender, BuildErrorEventArgs buildErrorEventArgs)
+	    {
+			buildLog.BuildErrors.Add(new BuildError
+				                         {
+					                         TimeStamp = buildErrorEventArgs.Timestamp,
+											 ErrorCode = buildErrorEventArgs.Code,
+											 FileName = buildErrorEventArgs.File,
+											 ProjectFile = buildErrorEventArgs.ProjectFile,
+											 Message = buildErrorEventArgs.Message,
+											 SubCategory = buildErrorEventArgs.Subcategory,
+											 LineNumber = buildErrorEventArgs.LineNumber,
+											 ColumnNumber = buildErrorEventArgs.ColumnNumber
+				                         });
+	    }
+
+	    private void OnProjectFinished(object sender, ProjectFinishedEventArgs projectFinishedEventArgs)
+	    {
+			buildLog.ProjectFinishEvents.Add(new ProjectFinishLog
+				                                 {
+					                                 FinishTime = projectFinishedEventArgs.Timestamp,
+													 IsBuildSucceeded = projectFinishedEventArgs.Succeeded,
+													 Message = projectFinishedEventArgs.Message,
+													 ProjectFile = projectFinishedEventArgs.ProjectFile
+				                                 });
+		}
+
+	    private void OnProjectStarted(object sender, ProjectStartedEventArgs projectStartedEventArgs)
+	    {
+			buildLog.ProjectStartEvents.Add(new ProjectStartLog
+				                                {
+					                                StartTime = projectStartedEventArgs.Timestamp,
+													Message = projectStartedEventArgs.Message,
+													ProjectFile = projectStartedEventArgs.ProjectFile
+				                                });
+		}
+
+	    private void OnBuildFinished(object sender, BuildFinishedEventArgs buildFinishedEventArgs)
+	    {
+		    buildLog.BuildEndTime = new BuildFinishLog
+			                            {
+				                            FinishTime = buildFinishedEventArgs.Timestamp,
+											IsBuildSucceeded = buildFinishedEventArgs.Succeeded,
+											Message = buildFinishedEventArgs.Message
+			                            };
+	    }
+
+	    private void OnBuildStarted(object sender, BuildStartedEventArgs buildStartedEventArgs)
+	    {
+		    buildLog.BuildStartTime = new BuildStartLog
+			                              {
+				                              StartTime = buildStartedEventArgs.Timestamp,
+											  Message = buildStartedEventArgs.Message
+			                              };
+	    }
+    }
+}
